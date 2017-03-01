0cdb6c41abeb6e7e9fd2c35409341c9f43bd5350
Menashe Gezelkopy <menashe.gezelkopy@gmail.com>
2017-03-02 01:19:42 +0200

    version 1.0.34

--- messages/index.js
+++ messages/index.js
@@ -667,6 +667,8 @@ bot.dialog('/myTickets', [
 
                var nresultLen = result.length;
 
+               session.send("results: " + nresultLen);
+
                for (var i=0; i<nresultLen; i++ ) {
 
                    session.send("results: " + result[i].ObjectTxt + " | " +  result[i].ObjectNo);
