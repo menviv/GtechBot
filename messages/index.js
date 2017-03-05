e673977b70d047d6319cfac751e5f78f23f28324
Menashe Gezelkopy <menashe.gezelkopy@gmail.com>
2017-03-05 22:06:01 +0200

    version 1.0.230

--- messages/index.js
+++ messages/index.js
@@ -318,7 +318,7 @@ bot.dialog('/validateUser', [
 
                         session.send("dddddddd: " + result);
 
-                        if (result[0].Name == undefined) {
+                        if (!result) {
 
                             session.userData.emailValidated == 'False';
 
